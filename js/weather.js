135d3e7a852afcf1e20edc72a85bc791a98339b9
bmbaron <benjamin.m.baron@gmail.com>
2017-01-25 19:51:26 +0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -43,8 +43,11 @@ function getWeatherAndDisplay() {
 
  
     //$.getJSON("http://api.openweathermap.org/data/2.5/weather?units=metric&lat=42&lon=73&APPID=eaf607284a508b29627f7369b57bc03d&callback=?",function(json){
-  var url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&APPID=eaf607284a508b29627f7369b57bc03d&callback=?";
-  alert(url);
+  var website = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + coordinates[0] + '&lon=' + coordinates[1] + '&APPID=eaf607284a508b29627f7369b57bc03d&callback=?';
+  alert('website');
+
+  alert(website);
+
 
   $.getJSON(url,function(json){
   
