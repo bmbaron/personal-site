9bb8ddf013e2b5d90325f49494f4baea1d6f953f
bmbaron <benjamin.m.baron@gmail.com>
2017-01-26 00:45:11 +0800

    Update indexQuotes.js

--- js/indexQuotes.js
+++ js/indexQuotes.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       
       type: 'GET',
       dataType: "jsonp",
-      url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+      url: "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
       jsonpCallback: "parseQuote" 
 
     });    
