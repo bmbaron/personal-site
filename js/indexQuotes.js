c7f0d1b3806a4a6d2b5201c25aa802a66414bb04
bmbaron <benjamin.m.baron@gmail.com>
2017-01-25 17:06:59 +0800

    Update indexQuotes.js

--- js/indexQuotes.js
+++ js/indexQuotes.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       
       type: 'GET',
       dataType: "jsonp",
-      url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&key=10&lang=en&jsonp=?",
+      url: "//api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&key=10&lang=en&jsonp=?",
       jsonpCallback: "parseQuote" 
 
     });    
